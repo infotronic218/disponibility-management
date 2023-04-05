863ba8d9f0afe585c1dedd29ea50f9ef0fbcb766
Sawadogo Ousseni <44531254+infotronic218@users.noreply.github.com>
2023-04-05 16:22:53 -0700

    first commit

--- obj/Debug/net6.0-windows/Login.g.i.cs
+++ obj/Debug/net6.0-windows/Login.g.i.cs
@@ -77,7 +77,7 @@ namespace ManagingAPP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ManagingAPP;V1.0.0.0;component/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ManagingAPP;component/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
